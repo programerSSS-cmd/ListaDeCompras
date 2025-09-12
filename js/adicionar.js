de22f36f3b73bfe947d7b4feaa80dc982497d7a6
Erick Rodrigues da Silva <erickgamerh2o@gmail.com>
2025-09-12 09:41:00 -0300

    Added elements(input) of quantity and value.

--- js/adicionar.js
+++ js/adicionar.js
@@ -7,14 +7,36 @@ adicionarClick.addEventListener('click', function adicionar() {
 
     //Primeira validação de entrada de dados
     if (prod.value != '') {
-        //criando um elemento <li>
+
+        //criando todos os elementos dinâmicamente e atrelando a lista
         let newProd = document.createElement('li')
-        //colocando uma classe para este elemento <li>
         newProd.setAttribute('class', 'newProd')
-        //inserindo o produto digitado no HTML do meu <li>
         newProd.innerHTML = prod.value
-        //inserindo meu novo produto na lista
         lista.appendChild(newProd)
+
+        let newQtd = document.createElement('input')
+        newQtd.setAttribute('class', 'newQtd')
+        newQtd.setAttribute('type', 'number')
+        newQtd.setAttribute('placeholder', 'Digite a Quantidade...')
+        newProd.appendChild(newQtd)
+
+        let newVlr = document.createElement('input')
+        newVlr.setAttribute('class', 'newVlr')
+         newVlr.setAttribute('type', 'number')
+        newVlr.setAttribute('placeholder', 'Digite o valor Unitario ...')
+        newProd.appendChild(newVlr)
+
+        let btnExcluir = document.createElement('button')
+        btnExcluir.setAttribute('class', 'btnExcluir')
+        btnExcluir.innerHTML = 'Excluir'
+        newProd.appendChild(btnExcluir)
+
+        //limpando e focando, campo para nova inserção de um novo produto
+        if (newProd) {
+            prod.value = ''
+            prod.focus()
+        }
+
     } else {
         alert('Nenhum produto encontrado, verifique e tente novamente ...')
     }
